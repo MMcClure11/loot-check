9e6665f130a866560f594be617569d89271d10cb
MMcClure11 <mcclme02@luther.edu>
2021-01-18 14:22:38 -0600

    Add test and component for Wallet

---
+++ src/components/Wallet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Wallet extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Wallet Balance</h3>
+      </div>
+    )
+  }
+}
---
+++ src/components/Wallet.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import Wallet from './Wallet';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+describe('Wallet', () => {
+  const wallet = shallow(<Wallet />)
+
+  it('renders properly', () => {
+    expect(wallet).toMatchSnapshot();
+  });
+});
