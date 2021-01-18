33f7fe8aebb1a1bb302e5d301eecea7461a55ae7
MMcClure11 <mcclme02@luther.edu>
2021-01-18 15:08:50 -0600

    Add test and reducer for withdraw

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -16,4 +16,12 @@ describe('balanceReducer', () => {
     expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit}))
       .toEqual(initialState + deposit);
   });
+
+  it('withdraws from the balance', () => {
+    const withdraw = 5;
+    const initialState = 15;
+
+    expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdraw}))
+      .toEqual(initialState - withdraw);
+  });
 });
