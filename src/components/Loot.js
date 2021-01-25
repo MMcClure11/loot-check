ede56d62c555ffc953d2de94457539b643e0b669
MMcClure11 <mcclme02@luther.edu>
2021-01-25 11:53:05 -0600

    WIP finished, but have not solved fetchBitcoin is not a function error when running Loot.test.js

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -3,25 +3,21 @@ import { connect } from 'react-redux';
 import { fetchBitcoin } from '../actions/bitcoin';
 
 export class Loot extends Component {
-
   componentDidMount() {
     this.props.fetchBitcoin();
   }
 
   computeBitcoin() {
-    const { bitcoin } = this.props
+    const { bitcoin } = this.props;
 
-    if(Object.keys(bitcoin).length === 0 ) return '';
+    if (Object.keys(bitcoin).length === 0) return '';
 
     return this.props.balance / parseInt(bitcoin.bpi.USD.rate.replace(',', ''), 10);
   }
 
   render() {
-    console.log(this.props)
     return (
-      <div>
-        <h3>Bitcoin balance: { this.computeBitcoin() }</h3>
-      </div>
+      <h3>Bitcoin balance: {this.computeBitcoin()}</h3>
     )
   }
 }
